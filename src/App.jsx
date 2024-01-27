52636b4da1dfbe0728b87c4180a4131b324df8e5
marshjaja <114920895+marshjaja@users.noreply.github.com>
2024-01-27 14:06:54 +0000

    fix: Add basename to actually visualize the correct url path

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
 	return (
 		<>
-			<BrowserRouter>
+			<BrowserRouter basename="/marcias-react-portfolio">
 				<main>
 					<Routes></Routes>
 				</main>
