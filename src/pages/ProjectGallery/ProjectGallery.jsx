bb4a9f8c38437a7e1846a48210fa7436ad51c746
marshjaja <114920895+marshjaja@users.noreply.github.com>
2024-01-30 12:54:24 +0000

    feat: Implemented the initial components for the Contact page, providing this user the ability to reach out. Successfully integrated EmailJS, enabling the contact form to directly send emails to my personal inbox, ensuring seamless communication.

--- src/pages/ProjectGallery/ProjectGallery.jsx
+++ src/pages/ProjectGallery/ProjectGallery.jsx
@@ -6,6 +6,10 @@ function ProjectGallery() {
 		<>
 			<div className="container">
 				<div className="row">
+					<h3 className="header-text text-center mb-4">
+						<span className="underline-text">Project Gallery</span>
+					</h3>
+
 					<div className="col-lg-4 mb-3">
 						<ProjectCard />
 					</div>
