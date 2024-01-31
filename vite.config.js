6702ce353b8b3c0abcc08a0dcd5a52e4674cf272
marshjaja <114920895+marshjaja@users.noreply.github.com>
2024-01-31 01:36:20 +0000

    add: update .gitignore and vite config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
 	base: "/marcias-react-portfolio/",
-	build: { outDir: "./build" },
+	build: {
+		outDir: "dist",
+	},
 	plugins: [react()],
 });
